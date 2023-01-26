95294c6630f6ad76bbe467b556a0bfa3ab52a0c5
PierrePocheron <pierre.pocheron@gmail.com>
2023-01-26 15:43:23 +0100

    feat(product): display product information (draft)

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="product">
+    <div class="product-img">
+      <img v-if="product.image" :src="product.image" :alt="product.name" />
+      <IconProduct v-else />
+    </div>
+    <div class="product-label">
+      {{ product.name }}
+    </div>
+    <p>{{ product.price }}</p>
+    <p>{{ product.grade }}</p>
+    <p>{{ product.description }}</p>
+    <p>{{ product.category }}</p>
+    <p>{{ product.rating.rate }}</p>
+    <p>{{ product.rating.count }}</p>
+    <div>
+      <IconCart />
+      <a>Add to cart</a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IconProduct from "@/components/IconProduct.vue";
+import IconCart from "@/components/IconCart.vue";
+import { computed } from "vue";
+
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+});
+
+const product = computed(() => props.product);
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Product</h1>
+    <div>
+      <ProductDetails :product="product" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProductDetails from "@/components/ProductDetails.vue";
+
+const product = {
+  name: "Product",
+  price: null,
+  grade: null,
+  description: null,
+  category: null,
+  rating: {
+    rate: null,
+    count: null,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
