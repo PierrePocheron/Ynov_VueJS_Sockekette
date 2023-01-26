b8928d58ad47a3f633e2ece7a8c97acd19aebe3f
Hugo-22 <hugo.denis22@gmail.com>
2023-01-26 10:39:25 +0100

    Init router et store

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,7 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <header>
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header>
-
-    <main>
-      <div>Main</div>
-    </main>
-  </div>
+  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
