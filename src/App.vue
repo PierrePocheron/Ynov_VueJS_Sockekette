8e5c18684ce7c3821f7794a6ccd20955ca4be01b
BeatlesBOB <beatlesbob74120@gmail.com>
2023-01-26 10:42:48 +0100

    Init Menu

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <script setup></script>
 
 <template>
@@ -6,14 +5,3 @@
 </template>
 
 <style scoped></style>
-=======
-<template>
-  <AppMenu />
-</template>
-
-<script setup>
-import AppMenu from "./components/03 - Organismes/AppMenu.vue";
-</script>
-
-<style lang="scss" scoped></style>
->>>>>>> Stashed changes
