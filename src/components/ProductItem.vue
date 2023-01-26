75e9339e8b131d3e31569c6c1a58d152ed73a409
PierrePocheron <pierre.pocheron@gmail.com>
2023-01-26 16:49:59 +0100

    feat: display list products with information & add to card

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
+  <div class="product">
     <h2>{{ product.title }}</h2>
-    <div></div>
+    <p>category : {{ product.category }}</p>
+    <p>price : {{ product.price }}</p>
+    <p>desc : {{ product.description }}</p>
+    <p>rating count: {{ product.rating.count }}</p>
+    <p>rating rate: {{ product.rating.rate }}</p>
+    <p>{{ product.title }}</p>
+    <div class="product-img">
+      <img v-if="product.image" :src="product.image" :alt="product.title" />
+      <IconProduct v-else />
+    </div>
+
+    <CartItemQuantity :item="product" />
+
+    <br />
   </div>
 </template>
 
 <script setup>
 import { computed } from "vue";
+import CartItemQuantity from "@/components/CartItemQuantity.vue";
+
+// import IconProduct from "@/components/IconProduct.vue";
 
 const props = defineProps({
   product: {
@@ -19,4 +35,18 @@ const product = computed(() => props.product);
 </script>
 
 <style lang="scss" scoped>
+.product {
+  background: #eee;
+  padding: 1rem;
+
+  gap: 3rem;
+  &-img {
+    flex: 0 0 auto;
+    background: #fff;
+    width: 10rem;
+    height: 10rem;
+    display: grid;
+    place-items: center;
+  }
+}
 </style>
