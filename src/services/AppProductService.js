0a942f3623d32ffcf90e27fd152fbe6acc9c08b3
Hugo-22 <hugo.denis22@gmail.com>
2023-01-27 09:48:23 +0100

    formattage des catégories (label et slug)

--- src/services/AppProductService.js
+++ src/services/AppProductService.js
@@ -1,4 +1,19 @@
+import { slugify } from "@/utils/slugify";
+
 export const getCategories = async () => {
   const res = await fetch("https://fakestoreapi.com/products/categories");
-  return await res.json();
+  const categoriesName = await res.json();
+
+  let categories = [];
+  Object.keys(categoriesName).forEach(function (name, index) {
+    categories = [
+      ...categories,
+      {
+        label: categoriesName[index],
+        slug: slugify(categoriesName[index]),
+      },
+    ];
+  });
+
+  return categories;
 };
