dd5590c338f4cc4ef4899c967f4f369f7a01a3fe
PierrePocheron <pierre.pocheron@gmail.com>
2023-02-06 10:06:43 +0100

    feat(ui): add padding y to message

--- src/views/AppPaymentView.vue
+++ src/views/AppPaymentView.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="container">
     <AppHeading tag="h1" level="primary">
-      Merci pour votre commande !
+      🛒 Merci pour votre commande !
     </AppHeading>
   </div>
 </template>
+
 <script setup>
 import AppHeading from "@/components/01 - Atoms/AppHeading.vue";
 </script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 8% 0 8% 0;
+}
+</style>
